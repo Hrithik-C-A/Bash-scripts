ad9b6f9850fe825e3e8b4cf3b17775752861ef49
Hrithik C A <hrithik-c-a@outlook.com>
2025-02-18 00:26:40 -0800

    updated

--- cli-args-v2.sh
+++ cli-args-v2.sh
@@ -2,7 +2,7 @@
 
 if [ $# -lt 2 ]
 then
-  echo "You must provide atleast 2 arguments."
+  echo "$0 error: you must provide atleast 2 arguments."
 else
   echo "The first argument is $1. The second argument is $2."
 fi  
