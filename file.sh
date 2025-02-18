510b4ee059dfa17f42246fee2c62833636c50179
Hrithik C A <hrithik-c-a@outlook.com>
2025-02-17 20:28:00 -0800

    file script v2

--- file.sh
+++ file.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-for file in *.docx
+# for file in *.docx
+# do
+#   echo "${file} is a stinky microsoft word file"
+# done  
+
+files=(*.docx)  # Assign matching files to an array
+
+for file in "${files[@]}"
 do
-  echo "${file} is a stinky microsoft word file"
-done  
+  echo "${file} is a stinky Microsoft Word file"
+done
